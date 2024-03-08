c2e9ac11285206425aa59248e77f1a7f5a2b423d
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-03-08 13:01:41 -0600

    feat: Create endpoints to get and post, only for testing

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,18 @@
 module.exports = function (app) {
+  // Testing server
   app.get("/", (req, res) => {
     res.json({ message: "it works!" });
+  });
+
+  // GET all applications
+  app.get("/applications", (req, res) => {
+    res.json({ message: "List of applications" });
+  });
+
+  // POST a new application
+  app.post("/applications", (req, res) => {
+    const newApplication = req.body;
+    console.log("\nNew application generated:\n", newApplication);
+    res.json({ message: "Application submited" });
   });
 };
